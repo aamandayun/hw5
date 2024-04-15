7d4c0dfd2c61a05301995fefc6ebae91336868bb
Amanda Yun <akyun@usc.edu>
2024-04-15 20:52:54 +0000

    adding file first submission

--- schedwork.cpp
+++ schedwork.cpp
@@ -20,7 +20,81 @@ using namespace std;
 static const Worker_T INVALID_ID = (unsigned int)-1;
 
 
+
 // Add prototypes for any helper functions here
+
+bool validSchedule(const DailySchedule& sched, const size_t d, const size_t m){
+    // vector<Worker_T> validShift(sched[0].size(), 0);
+    // for(const auto& day : sched){
+    //     for(Worker_T worker : day){
+    //         validShift[worker]++;
+    //         if(validShift[worker]>m){
+    //             return false;
+    //         }
+    //     }
+    // }
+
+    for(const auto& day : sched){
+        if(day.size() != d){
+            return false;
+        }
+    }
+
+    return true;
+}
+
+
+//keep track of slot
+//make helper return a boolean
+bool helper(const AvailabilityMatrix& avail, const size_t d, DailySchedule& sched, size_t day, size_t slot, std::map<Worker_T, int>& canWork){
+
+
+    bool result = false;
+    // cout <<"sched size: " <<sched.size()<<endl;
+    if(day == sched.size()){
+        return true;
+    }
+
+    for(unsigned int id = 0U; id<avail[day].size(); id++){ 
+//find(first, last, value)
+        auto temp = sched[day].begin()+slot;
+
+        if(temp == std::find(sched[day].begin(), temp, id)){
+            //check if worker at id has shifts left AND if they can work that day
+            if(canWork[id]>0 && avail[day][id]==true){
+
+                canWork[id]--;
+                sched[day][slot] = id;
+
+                //new day
+                if(slot==sched[day].size()-1){
+                    result = helper(avail, d, sched, day+1, 0U, canWork);
+                }else{ //new worker
+                    result = helper(avail, d, sched, day, slot+1, canWork);
+                }
+
+
+                if(result == true){
+                    return true;
+                }
+
+
+//reset
+                sched[day][slot] = INVALID_ID;
+                canWork[id]++;
+            }
+        }
+
+        // if(currentDay.size() < d && currentDay.size() < m){
+        //     currentDay.push_back(worker);
+        //     helper(avail, d, m, sched, day+1, currentDay);
+        //     currentDay.pop_back();
+        // }
+    }
+
+    // currentDay.clear();
+    // cout <<"after clear: "<<currentDay.size()<<endl;
+}
 
 
 // Add your implementation of schedule() and other helper functions here
@@ -39,7 +113,23 @@ bool schedule(
     // Add your code below
 
 
+    std::vector<Worker_T> currentDay(dailyNeed, INVALID_ID);
+    std::map<Worker_T, int> canWork;
 
+//new day for each in avail
+    for(size_t i=0; i<avail.size();i++){
+        sched.push_back(currentDay);
+    }
+
+    for(int i = 0; i<avail[0].size(); i++){
+        canWork.insert(std::make_pair(i, maxShifts));
+        cout << canWork[i]<<endl;
+    }
+
+    bool result = helper(avail, dailyNeed, sched, 0U, 0U, canWork);
+    
+
+    return result;
 
 }
 
--- wordle.cpp
+++ wordle.cpp
@@ -11,8 +11,87 @@
 #include "dict-eng.h"
 using namespace std;
 
+#include <unordered_map>
+
+
+
 
 // Add prototypes of helper functions here
+bool containsFloaters(string s, string floating){
+  std::unordered_map<char, int> freq;
+
+
+  for(char c : floating){
+    if(c != '-'){
+      if(s.find(c)==std::string::npos){
+        return false;
+      }
+      s.erase(s.find(c), 1);
+    }
+    
+  }
+
+  return true;
+}
+
+bool fixed(string s, string in, int &dashcount){
+  int pos = 0;
+  for(char c : in){
+    if(c!='-'){
+      if(s[pos]!=c){
+        dashcount = 0;
+        return false;
+      }
+    }else{
+      dashcount++;
+    }
+    
+    pos++;
+  }
+
+  return true;
+  
+}
+
+void helper(const std::string& in, const std::string& floating, const std::set<std::string>& dict, std::string& curr, std::set<std::string>& result, size_t pos, int dashcount){
+  if(pos == in.length()){
+    if(dict.find(curr)!=dict.end()){
+      result.insert(curr);
+    }
+
+    return;
+  }
+
+  if(in[pos]!='-'){
+    curr[pos] = in[pos];
+    helper(in, floating, dict, curr, result, pos+1, dashcount);
+  }
+  else{
+    string floatingCopy = floating;
+    // cout <<"dashcount: " <<dashcount<<endl;
+    // cout << floating.length()<<endl;
+
+    if(dashcount == floating.length() && floating.length()>0){ //changing from == to <=
+      for(char c : floating){
+        curr[pos] = c;
+        pos++;
+        floatingCopy.erase(floatingCopy.find(c), 1);
+        dashcount--; //added this
+        if(in[pos]!='-'){
+          break;
+        }
+        cout <<c<<endl;
+      }
+      helper(in, floatingCopy, dict, curr, result, pos, dashcount);
+    }
+    else{
+      for(char c = 'a'; c<='z'; ++c){
+        curr[pos] = c;
+        helper(in, floating, dict, curr, result, pos+1, dashcount);
+      }
+    }
+  }
+}
 
 
 // Definition of primary wordle function
@@ -22,7 +101,36 @@ std::set<std::string> wordle(
     const std::set<std::string>& dict)
 {
     // Add your code here
+    //compute all possible English language words that exist given a string
+    //containing the already guessed letters that are in the correct location
+    //floating = yellow, has letters that must be part of the word
+    // in = -i--m
+
+    std::set<std::string>result;
+    std::string curr(in.length(), ' ');
+    std::set<std::string> newDict;
+
+    int dashcount = 0;
+    int actualDashcount = 0;
+    cout << in<<endl;
+
+    if(in.length()==0){
+      return result;
+    }
+
+    for(string s: dict){
+      if(containsFloaters(s, floating) && s.length()==in.length() && fixed(s, in, dashcount)){
+        actualDashcount = dashcount;
+        newDict.insert(s);
+      }
+    }
+
+    helper(in, floating, newDict, curr, result, 0, actualDashcount);
+
+
+    return result;
 
 }
 
 // Define any helper functions here
+
